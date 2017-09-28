c04529ab5812998efbc7e0c77ad1f9f49ba44667
S1mile <916549258@qq.com>
2017-09-28 09:40:04 +0800

    文件添加作业文件

---
+++ zzz.java
@@ -0,0 +1,18 @@
+package cfb;
+
+public class zzz {
+		public static void main(String[]args)
+		{
+			int i,j;
+		for(i=1;i<=9;i++)
+		{
+			for(j=1;j<=9;j++)
+			{
+				if(i<=j)
+				System.out.print(i+"*"+j+"="+i*j+"\t");			
+			}	
+			System.out.print("\n");
+		}	
+		}	
+}
+
